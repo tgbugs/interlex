2f6cfd811498ad80198e395d0685009d9acb815a
Tom Gillespie <tgbugs@gmail.com>
2018-04-29 23:44:28 -0700

    historical added __init__ _curies and _uri helpers

---
+++ interlex/interlex_curies.py
@@ -0,0 +1,5 @@
+#from gevent import monkey
+#monkey.patch_all()
+
+from interlex import run_curies
+app = run_curies()
---
+++ interlex/interlex_uri.py
@@ -0,0 +1,5 @@
+#from gevent import monkey
+#monkey.patch_all()
+
+from interlex import run_uri
+app = run_uri()
