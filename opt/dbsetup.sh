31853488c66ba0b0a7cf429f6a9aae136f062661
Tom Gillespie <tgbugs@gmail.com>
2018-04-29 23:59:06 -0700

    dbsetup sql path

--- opt/dbsetup.sh
+++ opt/dbsetup.sh
@@ -1,3 +1,12 @@
+ABS_PATH="`dirname \"$0\"`"
+ABS_PATH="`( cd \"$ABS_PATH\" && pwd )`"
+if [ -z "$ABS_PATH" ] ; then
+  exit 1
+fi
+echo "$ABS_PATH"
+
+SQL="${ABS_PATH}/../sql/"
+
 if [ -z $1 ]; then
     PORT=5423
 else
@@ -11,18 +20,18 @@ else
 fi
 
 # postgres setup
-psql -U postgres -h localhost -p $PORT -d postgres  -f ilx-setup-postgres-postgres.sql -v database=$DATABASE
-psql -U postgres -h localhost -p $PORT -d $DATABASE -f ilx-setup-postgres-interlex.sql
+psql -U postgres -h localhost -p $PORT -d postgres  -f $SQL/ilx-setup-postgres-postgres.sql -v database=$DATABASE
+psql -U postgres -h localhost -p $PORT -d $DATABASE -f $SQL/ilx-setup-postgres-interlex.sql
 
 # interlex-admin setup
-psql -U interlex-admin -h localhost -p $PORT -d $DATABASE -f ilx-schema-interlex.sql
-psql -U interlex-admin -h localhost -p $PORT -d $DATABASE -f ilx-schema-groups.sql
-psql -U interlex-admin -h localhost -p $PORT -d $DATABASE -f ilx-schema-triple-tables.sql
-psql -U interlex-admin -h localhost -p $PORT -d $DATABASE -f ilx-schema-uri-mapping-tables.sql
-psql -U interlex-admin -h localhost -p $PORT -d $DATABASE -f ilx-schema-inserts.sql
-psql -U interlex-admin -h localhost -p $PORT -d $DATABASE -f ilx-schema-permissions.sql -v database=$DATABASE
+psql -U interlex-admin -h localhost -p $PORT -d $DATABASE -f $SQL/ilx-schema-interlex.sql
+psql -U interlex-admin -h localhost -p $PORT -d $DATABASE -f $SQL/ilx-schema-groups.sql
+psql -U interlex-admin -h localhost -p $PORT -d $DATABASE -f $SQL/ilx-schema-triple-tables.sql
+psql -U interlex-admin -h localhost -p $PORT -d $DATABASE -f $SQL/ilx-schema-uri-mapping-tables.sql
+psql -U interlex-admin -h localhost -p $PORT -d $DATABASE -f $SQL/ilx-schema-inserts.sql
+psql -U interlex-admin -h localhost -p $PORT -d $DATABASE -f $SQL/ilx-schema-permissions.sql -v database=$DATABASE
 
 # tests
-psql -U interlex-admin -h localhost -p $PORT -d $DATABASE -f ilx-schema-test.sql
-psql -U interlex-admin -h localhost -p $PORT -d $DATABASE -f ilx-schema-test-fail.sql
+psql -U interlex-admin -h localhost -p $PORT -d $DATABASE -f $SQL/ilx-schema-test.sql
+psql -U interlex-admin -h localhost -p $PORT -d $DATABASE -f $SQL/ilx-schema-test-fail.sql
 
