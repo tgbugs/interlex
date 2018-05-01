73e9bb6c0721f044f89bd7d9ffa054dcf19a2050
Tom Gillespie <tgbugs@gmail.com>
2018-05-01 11:43:45 -0700

    test_load fixed to use the new init method

--- test/test_load.py
+++ test/test_load.py
@@ -1,5 +1,6 @@
 import os
 import unittest
+from unittest.mock import MagicMock
 from pathlib import Path
 from pyontutils.config import devconfig  # FIXME this will cause issues down the line
 from interlex.exc import LoadError
@@ -11,14 +12,17 @@ class FakeResultProxy:
     name = 'no one actually checks this value'
     expected_bound_name = None
 
+def ident_exists(*args, **kwargs):
+    return False
+
 class TestLoader(unittest.TestCase):
     def test_loader(self):
         session = FakeSession()
-        FFF = FileFromFile(session)
-        FFF.session._return_value = (FakeResultProxy for _ in range(1))
+        myFileFromFile = FileFromFile(session)  # be careful, you only get to call this once
+        myFileFromFile.ident_exists = ident_exists
+        results = (FakeResultProxy for _ in range(999))
         #FFF.reference_host = 'uri.interlex.org'
         #embed()
-        fff = FFF()
         ttl = Path(devconfig.ontology_local_repo) / 'ttl'
         paths =  ('NIF-GrossAnatomy.ttl',
                   #'NIF-Chemical.ttl',
@@ -32,9 +36,12 @@ class TestLoader(unittest.TestCase):
         ebns = [os.path.join('http://ontology.neuinfo.org/NIF/ttl', p) for p in paths]
         name = names[0]
         for name, ebn in list(zip(names, ebns))[::-1]:
-            with fff as f:
-                setup_ok = f(name.as_posix(), ebn)
-                if setup_ok is not None:
-                    raise LoadError(setup_ok)
-                
-                out = f.load()  # TODO raise error on this one
+            session.execute = MagicMock(return_value=results)
+            #myFileFromFile.session.return_value = results
+            fff = myFileFromFile()
+            setup_ok = fff(name, ebn)
+            if setup_ok is not None:
+                raise LoadError(setup_ok)
+            #fff.process_graph()  # load calls this, but this is what is needed if you want the graph loaded but not sent to interlex
+            #fff.subgraph_identities  # after calling process_graph this will work
+            out = fff.load()  # TODO raise error on this one
