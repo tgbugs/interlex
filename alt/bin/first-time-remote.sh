652d001f6e9cb45520509dd0a0a18fcfa5ce1fa4
Tom Gillespie <tgbugs@gmail.com>
2018-09-28 14:50:10 -0700

    alt/bin ftr install pipenv and fail if ssh fails

--- alt/bin/first-time-remote.sh
+++ alt/bin/first-time-remote.sh
@@ -11,6 +11,11 @@ ssh ${INTERLEX_USER}@${INTERLEX_SERVER} "rm -rf run/;  # in case it is not actua
     sudo ln -s /etc/nginx/sites-available/uri.interlex.org.conf /etc/nginx/sites-enabled/uri.interlex.org.conf &&
     sudo systemd-tmpfiles --create &&
     sudo systemctl enable ilxalt &&
-    cd run && pipenv install && cd ~/ && touch .mypass && chmod 0600 .mypass"
+    pip3.6 install --user --ignore-installed pipenv &&
+    cd run &&
+    pipenv install &&
+    cd ~/ &&
+    touch .mypass &&
+    chmod 0600 .mypass" &&
 echo you need to edit ~/.mypass on ${INTERLEX_SERVER} as ${INTERLEX_USER} to complete the setup
 echo the pattern used to set the password is deocumented in step five of README.md on the server
