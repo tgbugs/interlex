4b3b752037a97dab6c21c57624a34a38c5fafe0b
Tom Gillespie <tgbugs@gmail.com>
2018-09-28 18:08:57 -0700

    alt/bin remote ssh fail and mv -f existing wheel file

--- alt/bin/remote.sh
+++ alt/bin/remote.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
-ssh ${INTERLEX_USER}@${INTERLEX_SERVER} "mv run/*.whl . ;
+ssh ${INTERLEX_USER}@${INTERLEX_SERVER} "mv -f run/*.whl . ;
     unzip -o alt.zip &&
     sudo systemctl stop ilxalt &&
     cd run &&
     ~/.local/bin/pipenv install *.whl &&
     ~/.local/bin/pipenv update &&
     sudo systemctl start ilxalt"
+if [ $? -ne 0 ]; then
+    exit 1
+fi
