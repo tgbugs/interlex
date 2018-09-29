35cd2a61862445cdcfb24ef1c3b007a8bbb005bc
Tom Gillespie <tgbugs@gmail.com>
2018-09-28 18:17:42 -0700

    alt/bin remote use pipenv --rm because pipenv doesn't actually update a whl install if the version is the same, sigh

--- alt/bin/remote.sh
+++ alt/bin/remote.sh
@@ -4,8 +4,8 @@ ssh ${INTERLEX_USER}@${INTERLEX_SERVER} "mv -f run/*.whl . ;
     unzip -o alt.zip &&
     sudo systemctl stop ilxalt &&
     cd run &&
+    ~/.local/bin/pipenv --rm &&
     ~/.local/bin/pipenv install *.whl &&
-    ~/.local/bin/pipenv update &&
     sudo systemctl start ilxalt"
 if [ $? -ne 0 ]; then
     exit 1
