192eb6ee365c43771ae02425e4a7d7b0aa287aae
Tom Gillespie <tgbugs@gmail.com>
2018-09-28 18:52:45 -0700

    alt/bin remote check if service fails shortly after start

--- alt/bin/remote.sh
+++ alt/bin/remote.sh
@@ -6,7 +6,11 @@ ssh ${INTERLEX_USER}@${INTERLEX_SERVER} "mv -f run/*.whl . ;
     cd run &&
     ~/.local/bin/pipenv --rm &&
     ~/.local/bin/pipenv install *.whl &&
-    sudo systemctl start ilxalt"
+    sudo systemctl start ilxalt &&
+    sleep .2
+    if [ -n $(sudo systemctl is-active --quiet ilxalt) ]; then
+        exit 1;
+    fi"
 if [ $? -ne 0 ]; then
     exit 1
 fi
