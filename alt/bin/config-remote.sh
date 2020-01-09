e7e63b8b9c3faaaa36d2fb9fefd8e6bb3064d12d
Tom Gillespie <tgbugs@gmail.com>
2020-01-09 15:22:25 -0800

    alt readme nowebify config-remote and remote
    also reorganize headings to clarify the process a bit

--- alt/bin/config-remote.sh
+++ alt/bin/config-remote.sh
@@ -1,30 +1,34 @@
 #!/usr/bin/env bash
 # [[file:~/git/interlex/alt/README.org::config-remote.sh][config-remote.sh]]
-ssh ${INTERLEX_USER}@${INTERLEX_SERVER} "rm -rf run/;
-    rm -rf deploy_files/;
-    unzip -o alt.zip &&
+ssh ${INTERLEX_USER}@${INTERLEX_SERVER} "
+# [[[[file:~/git/interlex/alt/README.org::config-remote-command][config-remote-command]]][config-remote-command]]
+rm -rf run/;
+rm -rf deploy_files/;
+unzip -o alt.zip &&
     sudo /bin/cp -f deploy_files/etc/systemd/system/ilxalt.service /etc/systemd/system/ &&
     sudo /bin/cp -f deploy_files/etc/systemd/system/ilxalt.socket /etc/systemd/system/ &&
     sudo /bin/cp -f deploy_files/etc/tmpfiles.d/ilxalt.conf /etc/tmpfiles.d/ &&
     sudo /bin/cp -f deploy_files/etc/nginx/sites-available/uri.interlex.org.conf /etc/nginx/sites-available/ && # carful here
     sudo unlink /etc/nginx/sites-enabled/uri.interlex.org.conf;
-    sudo ln -s /etc/nginx/sites-available/uri.interlex.org.conf /etc/nginx/sites-enabled/uri.interlex.org.conf &&
+sudo ln -s /etc/nginx/sites-available/uri.interlex.org.conf /etc/nginx/sites-enabled/uri.interlex.org.conf &&
     sudo systemd-tmpfiles --create &&
     sudo systemctl enable ilxalt &&
     pip3.6 install --user --ignore-installed pipenv &&
     cd run &&
     ~/.local/bin/pipenv --rm;  # the very first time this can fail
-    ~/.local/bin/pipenv install &&
+~/.local/bin/pipenv install &&
     cd ~/ &&
     touch .mypass &&
     chmod 0600 .mypass
-    if [ -s .mypass ]; then
-        sudo systemctl restart ilxalt &&
+if [ -s .mypass ]; then
+    sudo systemctl restart ilxalt &&
         sudo systemctl restart nginx;
-    else
-        echo ~/.mypass has no records;
-        exit 2;
-    fi"
+else
+    echo ~/.mypass has no records;
+    exit 2;
+fi
+# config-remote-command ends here
+"
 SSH_EXIT=$?
 if [ $SSH_EXIT -eq 2 ]; then
     echo you need to edit ~/.mypass on ${INTERLEX_SERVER} as ${INTERLEX_USER} to complete setup
--- alt/bin/remote.sh
+++ alt/bin/remote.sh
@@ -1,18 +1,22 @@
 #!/usr/bin/env bash
 # [[file:~/git/interlex/alt/README.org::remote.sh][remote.sh]]
-ssh ${INTERLEX_USER}@${INTERLEX_SERVER} "mv -f run/*.whl . ;
-    rm run/Pipenv.lock;
-    unzip -o alt.zip &&
+ssh ${INTERLEX_USER}@${INTERLEX_SERVER} "
+# [[[[file:~/git/interlex/alt/README.org::remote-command][remote-command]]][remote-command]]
+mv -f run/*.whl . ;
+rm run/Pipenv.lock;
+unzip -o alt.zip &&
     cd run &&
     ~/.local/bin/pipenv --rm;
-    ~/.local/bin/pipenv install *.whl &&
+~/.local/bin/pipenv install *.whl &&
     sudo systemctl restart ilxalt &&
     sleep 5
-    sudo systemctl is-active --quiet ilxalt
-    if [ $? -ne 0 ]; then
-        sudo journalctl -u ilxalt.service | tail -n 50;
-        exit 1;
-    fi"
+sudo systemctl is-active --quiet ilxalt
+if [ $? -ne 0 ]; then
+    sudo journalctl -u ilxalt.service -n 50;
+    exit 1;
+fi
+# remote-command ends here
+"
 SSH_EXIT=$?
 if [ $SSH_EXIT -ne 0 ]; then
     exit $SSH_EXIT
