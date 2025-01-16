9ff87814c8e53e2ff55a505276af1944b358d20f
Tom Gillespie <tgbugs@gmail.com>
2025-01-15 17:05:29 -0800

    sql first pass at comments

--- sql/permissions.sql
+++ sql/permissions.sql
@@ -11,7 +11,7 @@ GRANT UPDATE (own_role) ON interlex.groups TO "interlex-user";
 GRANT UPDATE (argon2_string) ON interlex.user_passwords TO "interlex-user";
 GRANT UPDATE (email_validated) ON interlex.user_emails TO "interlex-user";
 GRANT DELETE ON interlex.emails_validating TO "interlex-user";
-
 GRANT UPDATE (user_role) ON interlex.user_permissions TO "interlex-user";
+GRANT UPDATE (replaced) ON interlex.comments TO "interlex-user";
 
 GRANT UPDATE (expected_bound_name) ON interlex.reference_names TO "interlex-user";
