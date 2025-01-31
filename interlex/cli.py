67c12a0207293230a294ee72028ce4a8ca7c4c85
Tom Gillespie <tgbugs@gmail.com>
2025-01-30 16:40:51 -0800

    docs setup.org update to better reflect current processes
    a full operations manual for prod will be needed, for now the examples
    and tests in tgbugs/dockerfiles source.org are the best place to look

--- interlex/cli.py
+++ interlex/cli.py
@@ -142,18 +142,20 @@ class Main(clif.Dispatcher):
         pass
 
     def dbsetup(self):
+        raise NotImplementedError('TODO')
         from interlex.uri import run_uri
         from sqlalchemy.sql import text as sql_text
-        app = run_uri()  # database init happens inside run_uri now
+        app = run_uri()
         db = app.extensions['sqlalchemy']
         session = db.session
         sql_verify_user = (
             'INSERT INTO user_emails (user_id, email, email_primary) VALUES (:id, :email, :email_primary);'
             'INSERT INTO user_orcid (user_id, orcid) VALUES (:id, :orcid);'
         )
-        args_verify_user = dict(id=1,
-                             orcid='https://orcid.org/0000-0002-7509-4801',
-                             email='tgbugs@gmail.com', email_primary=True)
+        args_verify_user = dict(
+            id=1,
+            orcid='https://orcid.org/0000-0002-7509-4801',
+            email='tgbugs@gmail.com', email_primary=True)
         with app.app_context():
             session.execute(sql_text(sql_verify_user), args_verify_user)
             session.commit()
